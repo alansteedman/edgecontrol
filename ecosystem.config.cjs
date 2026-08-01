a93c28d4bed640f696e1ae8bcdfd861f5e47747e
Alan Steedman <alansteedman@gmail.com>
2026-08-01 12:22:00 +0100

    fix: increase UV_THREADPOOL_SIZE to 16, remove WS ping disconnect

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
     name: 'edgecontroller',
     script: './server.js',
     env: {
-      PROVISION_KEY: '8bc536e97a3996ea7b660e400c321e3132772ec0a5ac50367c24309391014fa9'
+      PROVISION_KEY: '8bc536e97a3996ea7b660e400c321e3132772ec0a5ac50367c24309391014fa9',
+      UV_THREADPOOL_SIZE: '16'
     }
   }]
 }
